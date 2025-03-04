289762e521a3069ac7f91eafb3d49f9bd859d89d
Z4ck <128758112+Z4ckStudios@users.noreply.github.com>
2025-03-04 14:33:44 -0700

    Ciphered - Alpha v0.01
    - Added content

---
+++ Javascript!Gmtlivih.js
@@ -0,0 +1,115 @@
+var url = "https://Z4ckStudios.github.io/Ciphered/Gmtlivih";
+window.history.replaceState(null, document.title, url);
+
+var PageLoaded = localStorage.getItem("PageLoaded");
+localStorage.setItem("PageLoaded", "true");
+
+// if(PageLoaded == "true")
+// location.replace("index.html")
+
+var ecodebox = document.querySelector(".ecodebox");
+var dcodebox = document.querySelector(".dcodebox");
+var ecodebutton = document.querySelector(".ecodebutton");
+
+ecodebutton.addEventListener("click",
+function(e) {
+
+    var list = {
+        A: "rgb(255, 255, 255)" + " ",
+        a: "rgb(255, 255, 255)" + " ",
+        B: "rgb(0, 0, 180)" + " ",
+        b: "rgb(0, 0, 180)" + " ",
+        C: "rgb(175, 13, 102)" + " ",
+        c: "rgb(175, 13, 102)" + " ",
+        D: "rgb(146, 248, 70)" + " ",
+        d: "rgb(146, 248, 70)" + " ",
+        E: "rgb(255, 200, 47)" + " ",
+        e: "rgb(255, 200, 47)" + " ",
+        F: "rgb(255, 118, 0)" + " ",
+        f: "rgb(255, 118, 0)" + " ",
+        G: "rgb(255, 128, 0)" + " ",
+        g: "rgb(255, 128, 0)" + " ",
+        H: "rgb(185, 185, 185)" + " ",
+        h: "rgb(185, 185, 185)" + " ",
+        I: "rgb(235, 235, 222)" + " ",
+        i: "rgb(235, 235, 222)" + " ",
+        J: "rgb(100, 100, 100)" + " ",
+        j: "rgb(100, 100, 100)" + " ",
+        K: "rgb(255, 255, 0)" + " ",
+        k: "rgb(255, 255, 0)" + " ",
+        L: "rgb(55, 19, 112)" + " ",
+        l: "rgb(55, 19, 112)" + " ",
+        M: "rgb(255, 255, 150)" + " ",
+        m: "rgb(255, 255, 150)" + " ",
+        N: "rgb(202, 62, 94)" + " ",
+        n: "rgb(202, 62, 94)" + " ",
+        O: "rgb(205, 145, 63)" + " ",
+        o: "rgb(205, 145, 63)" + " ",
+        P: "rgb(12, 75, 100)" + " ",
+        p: "rgb(12, 75, 100)" + " ",
+        Q: "rgb(255, 0, 0)" + " ",
+        q: "rgb(255, 0, 0)" + " ",
+        R: "rgb(175, 155, 50)" + " ",
+        r: "rgb(175, 155, 50)" + " ",
+        S: "rgb(175, 155, 50)" + " ",
+        s: "rgb(175, 155, 50)" + " ",
+        T: "rgb(37, 70, 25)" + " ",
+        t: "rgb(37, 70, 25)" + " ",
+        U: "rgb(37, 70, 25)" + " ",
+        u: "rgb(37, 70, 25)" + " ",
+        V: "rgb(121, 33, 135)" + " ",
+        v: "rgb(121, 33, 135)" + " ",
+        W: "rgb(83, 140, 208)" + " ",
+        w: "rgb(83, 140, 208)" + " ",
+        X: "rgb(83, 140, 208)" + " ",
+        x: "rgb(83, 140, 208)" + " ",
+        Y: "rgb(0, 154, 37)" + " ",
+        y: "rgb(0, 154, 37)" + " ",
+        Z: "rgb(178, 220, 205)" + " ",
+        z: "rgb(178, 220, 205)" + " ",
+
+        0: "rgb(255, 0, 229)" + " ",
+        1: "rgb(229, 0, 229)" + " ",
+        2: "rgb(229, 255, 229)" + " ",
+        3: "rgb(229, 255, 255)" + " ",
+        4: "rgb(229, 255, 0)" + " ",
+        5: "rgb(0, 255, 0)" + " ",
+        6: "rgb(0, 229, 0)" + " ",
+        7: "rgb(0, 229, 229)" + " ",
+        8: "rgb(0, 229, 255)" + " ",
+        9: "rgb(255, 229, 255)" + " ",
+
+        ".": "rgb(229, 229, 229)" + " ",
+        ",": "rgb(0, 0, 0)" + " ",
+
+        " ": "",
+        ":": "",
+        ";": "",
+        ")": "",
+        "(": "",
+        "[": "",
+        "]": "",
+        "{": "",
+        "}": "",
+        "!": "",
+    };
+
+    var input = ecodebox.innerHTML;
+    var repECODE = input.replace
+    (/a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z|0|1|2|3|4|5|6|7|8|9|.|,|!/gi,
+    function (match) {return list[match];})
+    dcodebox.innerHTML = repECODE;
+})
+ecodebutton.addEventListener("mouseover",
+function() {
+
+    ecodebutton.style.background = "#ffffff";
+    ecodebutton.style.color = "#000000";
+    ecodebutton.style.cursor = "pointer";
+})
+ecodebutton.addEventListener("mouseout",
+function() {
+
+    ecodebutton.style.background = "#000000";
+    ecodebutton.style.color = "#ffffff";
+})
---
+++ Javascript!index.js
@@ -0,0 +1,164 @@
+var url = "https://Z4ckStudios.github.io/Ciphered/";
+window.history.replaceState(null, document.title, url);
+
+var CurrentPos = localStorage.getItem("CurrentPos");
+localStorage.setItem("PageLoaded", "false");
+
+var CipheredZS = document.querySelector(".GmtlivihZS");
+
+var list = [
+    "G_",//1
+    "Gm_",//2
+    "Gmt_",//3
+    "Gmtl_",//4
+    "Gmtli_",//5
+    "Gmtliv_",//6
+    "Gmtlivi_",//7
+    "Gmtlivih_",//8
+    "Gmtlivih _",//9
+    "Gmtlivih -_",//10
+    "Gmtlivih - _",//11
+    "Gmtlivih - D_",//12
+    "Gmtlivih - D4_",//13
+    "Gmtlivih - D4g_",//14
+    "Gmtlivih - D4go_",//15
+    "Gmtlivih - D4go _",//16
+    "Gmtlivih - D4go W_",//17
+    "Gmtlivih - D4go Wx_",//18
+    "Gmtlivih - D4go Wxy_",//19
+    "Gmtlivih - D4go Wxyh_",//20
+    "Gmtlivih - D4go Wxyhm_",//21
+    "Gmtlivih - D4go Wxyhms_",//22
+];
+var LGZS = 22;
+
+var listTransform1 = [
+    "Gmtlivih - D4go Wxyhmsw",//17
+    "Gmtlivih - D4go Wxyhmsv",//16
+    "Gmtlivih - D4go Wxyhmrv",//15
+    "Gmtlivih - D4go Wxyhlrv",//14
+    "Gmtlivih - D4go Wxyglrv",//13
+    "Gmtlivih - D4go Wxxglrv",//12
+    "Gmtlivih - D4go Wwxglrv",//11
+    "Gmtlivih - D4gn Vwxglrv",//10
+    "Gmtlivih - D4fn Vwxglrv",//9
+    "Gmtlivih - C4fn Vwxglrv",//8
+    "Gmtlivig - C4fn Vwxglrv",//7
+    "Gmtlivhg - C4fn Vwxglrv",//6
+    "Gmtliuhg - C4fn Vwxglrv",//5
+    "Gmtlhuhg - C4fn Vwxglrv",//4
+    "Gmtkhuhg - C4fn Vwxglrv",//3
+    "Gmskhuhg - C4fn Vwxglrv",//2
+    "Glskhuhg - C4fn Vwxglrv",//1
+];
+var LT1 = 17;
+
+var listTransform2 = [
+    "Flskhuhg - C4fn Vwxglru",//17
+    "Flskhuhg - C4fn Vwxglqu",//16
+    "Flskhuhg - C4fn Vwxgkqu",//15
+    "Flskhuhg - C4fn Vwxfkqu",//14
+    "Flskhuhg - C4fn Vwwfkqu",//13
+    "Flskhuhg - C4fn Vvwfkqu",//12
+    "Flskhuhg - C4fn Uvwfkqu",//11
+    "Flskhuhg - C4fm Uvwfkqu",//10
+    "Flskhuhg - C4em Uvwfkqu",//9
+    "Flskhuhg - B4em Uvwfkqu",//8
+    "Flskhuhf - B4em Uvwfkqu",//7
+    "Flskhugf - B4em Uvwfkqu",//6
+    "Flskhtgf - B4em Uvwfkqu",//5
+    "Flskgtgf - B4em Uvwfkqu",//4
+    "Flsjgtgf - B4em Uvwfkqu",//3
+    "Flrjgtgf - B4em Uvwfkqu",//2
+    "Fkrjgtgf - B4em Uvwfkqu",//1
+];
+var LT2 = 17;
+
+var listTransform3 = [
+    "Ekrjgtgf - B4em Uvwfkqt",//17
+    "Ekrjgtgf - B4em Uvwfkpt",//16
+    "Ekrjgtgf - B4em Uvwfjpt",//15
+    "Ekrjgtgf - B4em Uvwejpt",//14
+    "Ekrjgtgf - B4em Uvvejpt",//13
+    "Ekrjgtgf - B4em Uuvejpt",//12
+    "Ekrjgtgf - B4em Tuvejpt",//11
+    "Ekrjgtgf - B4el Tuvejpt",//10
+    "Ekrjgtgf - B4dl Tuvejpt",//9
+    "Ekrjgtgf - A4dl Tuvejpt",//8
+    "Ekrjgtge - A4dl Tuvejpt",//7
+    "Ekrjgtfe - A4dl Tuvejpt",//6
+    "Ekrjgsfe - A4dl Tuvejpt",//5
+    "Ekrjfsfe - A4dl Tuvejpt",//4
+    "Ekrifsfe - A4dl Tuvejpt",//3
+    "Ekqifsfe - A4dl Tuvejpt",//2
+    "Ejqifsfe - A4dl Tuvejpt",//1
+];
+var LT3 = 17;
+
+var listTransform4 = [
+    "Djqifsfe - A4dl Tuvejps",//17
+    "Djqifsfe - A4dl Tuvejos",//16
+    "Djqifsfe - A4dl Tuveios",//15
+    "Djqifsfe - A4dl Tuvdios",//14
+    "Djqifsfe - A4dl Tuudios",//13
+    "Djqifsfe - A4dl Ttudios",//12
+    "Djqifsfe - A4dl Studios",//11
+    "Djqifsfe - A4dk Studios",//10
+    "Djqifsfe - A4ck Studios",//9
+    "Djqifsfe - Z4ck Studios",//8
+    "Djqifsfd - Z4ck Studios",//7
+    "Djqifsed - Z4ck Studios",//6
+    "Djqifred - Z4ck Studios",//5
+    "Djqiered - Z4ck Studios",//4
+    "Djqhered - Z4ck Studios",//3
+    "Djphered - Z4ck Studios",//2
+    "Diphered - Z4ck Studios",//1
+];
+var LT4 = 17;
+
+var timer = 0;
+const timerInterval = setInterval(timerInt, 25);
+
+function timerInt() {
+    timer++
+
+    if(timer > 0) {CipheredZS.innerHTML = "_";}
+    if(timer > 20) {CipheredZS.innerHTML = "";}
+    if(timer > 40) {CipheredZS.innerHTML = "_";}
+    if(timer > 60) {CipheredZS.innerHTML = "";}
+    if(timer > 80) {CipheredZS.innerHTML = "_";}
+
+    if(timer > 100)
+    {LGZS = LGZS < list.length ? ++LGZS : LGZS - LGZS;
+    CipheredZS.innerHTML = list[LGZS];}
+
+    if(timer > 122) {CipheredZS.innerHTML = "Gmtlivih - D4go Wxyhmsw";}
+    
+    if(timer > 130)
+    {LT1 = LT1 < listTransform1.length ? ++LT1 : LT1 - LT1;
+    CipheredZS.innerHTML = listTransform1[LT1];}
+
+    if(timer > 147) {CipheredZS.innerHTML = "Flskhuhg - C4fn Vwxglrv";}
+
+    if(timer > 150)
+    {LT2 = LT2 < listTransform2.length ? ++LT2 : LT2 - LT2;
+    CipheredZS.innerHTML = listTransform2[LT2];}
+
+    if(timer > 167) {CipheredZS.innerHTML = "Ekrjgtgf - B4em Uvwfkqu";}
+
+    if(timer > 170)
+    {LT3 = LT3 < listTransform3.length ? ++LT3 : LT3 - LT3;
+    CipheredZS.innerHTML = listTransform3[LT3];}
+
+    if(timer > 187) {CipheredZS.innerHTML = "Djqifsfe - A4dl Tuvejpt";}
+
+    if(timer > 190)
+    {LT4 = LT4 < listTransform4.length ? ++LT4 : LT4 - LT4;
+    CipheredZS.innerHTML = listTransform4[LT4];}
+
+    if(timer > 207) {CipheredZS.innerHTML = "Ciphered - Z4ck Studios";}
+
+    if(timer > 300)
+    {location.replace("Gmtlivih.html");
+    clearInterval(timerInterval);}
+}
